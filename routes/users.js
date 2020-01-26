31aa8e7f2e65f3e2d41da84095a36cd931ba2ba8
Vincent Hullaert <hullaert.vincent@gmail.com>
2020-01-26 19:48:17 +0100

    feat(users): add method to add user
    use expres.json for parsing body as JSON

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+/* add user. */
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  res.send(req.body);
+});
+
 module.exports = router;
